9a4582e8ce6ca6d917b502f5a073a9bb9d4584a7
Charlie Williams <c@charliewilliams.org>
2017-11-23 20:42:43 +0000

    Outputting to csv

--- plates_and_dates.swift
+++ plates_and_dates.swift
@@ -2,28 +2,46 @@
 
 import Foundation
 
-@discardableResult
-func shell(_ args: String...) -> Int32 {
+func shell(launchPath: String, arguments: [String]) -> String {
+
     let task = Process()
-    task.launchPath = "/usr/bin/env"
-    task.arguments = args
+    task.launchPath = launchPath
+    task.arguments = arguments
+
+    let pipe = Pipe()
+    task.standardOutput = pipe
     task.launch()
-    task.waitUntilExit()
-    return task.terminationStatus
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: String.Encoding.utf8)!
+    if output.count > 0 {
+        //remove newline character.
+        let lastIndex = output.index(before: output.endIndex)
+        return String(output[output.startIndex ..< lastIndex])
+    }
+    return output
+}
+
+@discardableResult
+func bash(_ command: String, arguments: [String]) -> String {
+    let whichPathForCommand = shell(launchPath: "/bin/bash", arguments: [ "-l", "-c", "which \(command)" ])
+    return shell(launchPath: whichPathForCommand, arguments: arguments)
 }
 
 print("Starting up…")
 
-let alprCommand = "alpr -c gb -n 1 " // --config config/gb.conf  // append filename
+let alprCommand = "-c gb --config config/gb.conf -n 1" // append filename
 let fileManager = FileManager.default
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
 
 let sampleDir = "sampledata"
 var realDir: String?
 
 for arg in CommandLine.arguments {
-  if arg == "-d" || arg == "--data-directory" {
-    realDir = arg
-  }
+    if arg == "-d" || arg == "--data-directory" {
+        realDir = arg
+    }
 }
 
 private func creationDateForFile(atPath path: String) -> Date {
@@ -32,7 +50,7 @@ private func creationDateForFile(atPath path: String) -> Date {
     do {
         let attrs = try FileManager.default.attributesOfItem(atPath: path) as [FileAttributeKey:Any]
         if let creationDate = attrs[FileAttributeKey.creationDate] as? Date {
-          created = creationDate
+            created = creationDate
         }
 
     } catch let e {
@@ -41,32 +59,49 @@ private func creationDateForFile(atPath path: String) -> Date {
     return created
 }
 
-func readFiles(inDirectory dirPath: String) -> [(Date, String)] {
+func readFiles(inDirectory dirPath: String) -> [(date: Date, path: String)] {
 
-  guard let files = try? fileManager.contentsOfDirectory(atPath: dirPath) else {
-    print("Error, no files or path invalid")
-    return []
-  }
-  let basePath = "\(fileManager.currentDirectoryPath)/\(dirPath)"
+    guard let files = try? fileManager.contentsOfDirectory(atPath: dirPath) else {
+        print("Error, no files or path invalid")
+        return []
+    }
+    let basePath = "\(fileManager.currentDirectoryPath)/\(dirPath)"
 
-  var data = [(Date, String)]()
+    var data = [(Date, String)]()
 
-  for file in files {
-    let fullPath = "\(basePath)/\(file)"
-    data.append((creationDateForFile(atPath: fullPath), "\(dirPath)/\(file)"))
-  }
+    for file in files {
+        let fullPath = "\(basePath)/\(file)"
+        data.append((creationDateForFile(atPath: fullPath), "\(dirPath)/\(file)"))
+    }
 
-  print("Got data: ")
-  data.forEach({print($0)})
-  return data
+//    print("Got data: ")
+//    data.forEach({print($0)})
+    return data
 }
 
-func writeJSON() {
-
-}
-
-
-
+var data = "Date, Image file, Reg, Confidence, Notes,, \n"
 for dateAndFile in readFiles(inDirectory: realDir ?? sampleDir) {
-  shell(alprCommand + dateAndFile.1)
+
+    var commands = alprCommand.components(separatedBy: " ")
+    commands.append(dateAndFile.path)
+    let result = bash("alpr", arguments: commands)
+    let trimmed = result.replacingOccurrences(of: "plate0: 1 results", with: "")
+        .replacingOccurrences(of: "sampledata/", with: "")
+        .replacingOccurrences(of: "- ", with: "")
+        .replacingOccurrences(of: "\n", with: "")
+        .replacingOccurrences(of: "No license plates found.", with: ",, No license plates found.")
+        .replacingOccurrences(of: "confidence: ", with: ", ")
+    let dateString = dateFormatter.string(from: dateAndFile.date)
+    print(dateString, dateAndFile.path, trimmed)
+
+    data += "\(dateString),  \(dateAndFile.path),  \(trimmed),, \n"
 }
+
+do {
+    let url = URL(fileURLWithPath: "output.csv")
+    try data.write(to: url, atomically: false, encoding: .utf8)
+} catch let e {
+    print(e)
+}
+
+bash("open", arguments: ["output.csv"])
